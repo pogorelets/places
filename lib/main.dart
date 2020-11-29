234432f16079ce87b31b84e4fd12ce6799c8f7bb
lenap <pogorelets.elena@mail.ru>
2020-11-29 14:44:26 +0300

    pubspec include flutter_svg and font Roboto

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        fontFamily: 'Roboto',
+
         // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see the
