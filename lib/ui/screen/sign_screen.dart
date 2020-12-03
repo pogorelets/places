621651f742f8549719f9b83c8c0d7082fbc39014
lenap <pogorelets.elena@mail.ru>
2020-12-03 17:52:46 +0300

    task 4.2.2 RichText

--- lib/ui/screen/sign_screen.dart
+++ lib/ui/screen/sign_screen.dart
@@ -9,20 +9,30 @@ class _SignLightScreenState extends State<SignLightScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
         appBar: AppBar(
           toolbarHeight: 112,
           backgroundColor: Colors.transparent,
           elevation: 0.0,
           title: Padding(
             padding: EdgeInsets.only(top: 40.0),
-            child: Text(
-              "Список\nинтересных мест",
-              textAlign: TextAlign.left,
-              style: TextStyle(color: const Color(0xFF3B3E5B),
-              fontSize: 32,
-
-              ),
+            child: RichText(
+              text: TextSpan(
+                  text: "C",
+                  style: TextStyle(color: Colors.green, fontSize: 32),
+                  children: [
+                    TextSpan(
+                      text: "писок\n",
+                      style: TextStyle(
+                          color: const Color(0xFF3B3E5B), fontSize: 32),
+                    ),
+                    TextSpan(
+                        text: "И",
+                        style: TextStyle(color: Colors.yellow, fontSize: 32)),
+                    TextSpan(
+                        text: "нтересных мест",
+                        style: TextStyle(
+                            color: const Color(0xFF3B3E5B), fontSize: 32))
+                  ]),
             ),
           ),
         ),
