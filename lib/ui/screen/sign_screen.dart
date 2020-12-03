a73f1e13c314841fdbc8f20ffd41421a9c769e64
lenap <pogorelets.elena@mail.ru>
2020-12-03 15:51:45 +0300

    task 4.2.1 Text

--- lib/ui/screen/sign_screen.dart
+++ lib/ui/screen/sign_screen.dart
@@ -9,9 +9,25 @@ class _SignLightScreenState extends State<SignLightScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("First Scaffold"),),
+
+        appBar: AppBar(
+          toolbarHeight: 112,
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          title: Padding(
+            padding: EdgeInsets.only(top: 40.0),
+            child: Text(
+              "Список\nинтересных мест",
+              textAlign: TextAlign.left,
+              style: TextStyle(color: const Color(0xFF3B3E5B),
+              fontSize: 32,
+
+              ),
+            ),
+          ),
+        ),
         body: Container(
-      child: Center(child: Text("Sign light screen")),
-    ));
+          child: Center(child: Text("Sign light screen")),
+        ));
   }
 }
